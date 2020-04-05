93d8a06a05af62fab155cc62b3b730cf5fb31c5f
Tony Lindgren <tony@atomide.com>
2020-04-05 16:12:37 -0700

    motorolamodem: Add SMS PDU receive and use qmimodem for sending
    Looks like qmimodem sms is unable to re-route incoming messages for
    receiving on motorolamodem. The message notifications seem to be
    hardcoded to always show up as PDUs on DLC9 on the TS 27.010 serial
    interface.
    
    Let's use motorolamodem only to receive sms, and just let qmimodem
    handle the outgoing sms.
    
    Some parts of this patch are based on earlier patches done by
    Pavel Machek <pavel@ucw.cz>.

--- drivers/motorolamodem/motorolamodem.h
+++ drivers/motorolamodem/motorolamodem.h
@@ -21,3 +21,14 @@
 
 extern void motorola_voicecall_init(void);
 extern void motorola_voicecall_exit(void);
+
+extern void motorola_sms_init(void);
+extern void motorola_sms_exit(void);
+
+struct ofono_sms;
+
+struct motorola_sms_params {
+	struct ofono_sms *qmi_sms;
+	GAtChat *recv;
+	GAtChat *xmit;
+};
