389e2344f86319265fb72ae590b470716e038fdc
Sicelo A. Mhlongo <absicsz@gmail.com>
2024-12-17 11:31:29 +0200

    ussd: ensure ussd content fits in buffers
    Fixes: CVE-2024-7539

--- drivers/huaweimodem/ussd.c
+++ drivers/huaweimodem/ussd.c
@@ -38,7 +38,7 @@ static void cusd_parse(GAtResult *result, struct ofono_ussd *ussd)
 	int status;
 	int dcs = 0;
 	const char *content;
-	unsigned char msg[160];
+	unsigned char msg[160] = {0};
 	const unsigned char *msg_ptr = NULL;
 	long msg_len;
 
@@ -54,6 +54,9 @@ static void cusd_parse(GAtResult *result, struct ofono_ussd *ussd)
 		goto out;
 
 	g_at_result_iter_next_number(&iter, &dcs);
+
+	if (strlen(content) > sizeof(msg) * 2)
+		goto out;
 
 	msg_ptr = decode_hex_own_buf(content, -1, &msg_len, 0, msg);
 
